1b625f51ce07e3ad31468b5dbc779ae69367bc22
echohack <echohack@users.noreply.github.com>
2018-05-29 17:19:55 -0700

    Add Terraform and fix shell
    Signed-off-by: echohack <echohack@users.noreply.github.com>

---
+++ terraform/aws/outputs.tf
@@ -0,0 +1,10 @@
+# output "public_ips_initial_peer" {
+#   value = "${aws_instance.initial-peer.public_ip}"
+# }
+# output "public_ips_national-parks" {
+#   value = "${aws_instance.national-parks.public_ip}"
+# }
+# output "public_ips_np-mongodb" {
+#   value = "${aws_instance.np-mongodb.public_ip}"
+# }
+
