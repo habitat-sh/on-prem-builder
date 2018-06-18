fc5e8fa34d5b43bcfde2377122b290772a79f74b
Ryan <bonnerrk@gmail.com>
2018-06-18 09:27:27 -0400

    removed install/uninstall scripts from old location

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pushd scripts > /dev/null
-sudo -E ./install-hab.sh
-sudo -E ./hab-sup.service.sh
-sudo -E ./provision.sh
-popd > /dev/null
--- uninstall.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-hab svc status | tail -n +2 | grep "^habitat\/" | awk -F'/' '{print $2}' | xargs -I{} hab svc stop habitat/{}
-hab svc status | tail -n +2 | grep "^habitat\/" | awk -F'/' '{print $2}' | xargs -I{} hab svc unload habitat/{}
-rm -rf /hab/pkgs/habitat
