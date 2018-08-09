b87889259ffd5fb39a6a9d2e1cbd3fa809e0a377
Isaac Ma <isaac.ma@indellient.com>
2018-08-09 14:38:56 -0400

    Adding private IPs as output for habitat depot

--- terraform/aws/outputs.tf
+++ terraform/aws/outputs.tf
@@ -2,6 +2,10 @@ output "public_ips" {
   value = "${aws_instance.builder.public_ip}"
 }
 
+output "private_ips" {
+  value = "${aws_instance.builder.private_ip}"
+}
+
 output "depot_instance_id" {
   value = "${aws_instance.builder.id}"
 }
