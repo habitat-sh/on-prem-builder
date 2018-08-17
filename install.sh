32118305f98db35775ac92d805ff7de7a636c729
Salim Alam <salam@chef.io>
2018-08-17 16:20:21 -0700

    Add installation notice capability
    Signed-off-by: Salim Alam <salam@chef.io>

--- install.sh
+++ install.sh
@@ -2,8 +2,16 @@
 
 umask 0022
 
-pushd scripts > /dev/null
-sudo -E ./install-hab.sh
-sudo -E ./hab-sup.service.sh
-sudo -E ./provision.sh
-popd > /dev/null
+type curl >/dev/null 2>&1 || { echo >&2 "curl is required for installation of habitat, but was not found. Exiting."; exit 1; }
+
+curl https://raw.githubusercontent.com/habitat-sh/on-prem-builder/master/NOTICE
+
+echo
+read -r -p "Continue with installation? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
+    pushd scripts > /dev/null
+    sudo -E ./install-hab.sh
+    sudo -E ./hab-sup.service.sh
+    sudo -E ./provision.sh
+    popd > /dev/null
+fi
