e7f14af2dcc307c7c1a34392f565b5d9e3f59f47
Josh Black <raskchanky@gmail.com>
2018-12-21 15:50:38 -0800

    Add new license to install script
    Signed-off-by: Josh Black <raskchanky@gmail.com>

--- install.sh
+++ install.sh
@@ -10,9 +10,19 @@ sudo () {
 type curl >/dev/null 2>&1 || { echo >&2 "curl is required for installation of habitat, but was not found. Exiting."; exit 1; }
 
 curl https://raw.githubusercontent.com/habitat-sh/on-prem-builder/master/NOTICE
-
 echo
-read -r -p "Continue with installation? [y/N] " response
+
+license="${HAB_LICENSE:-}"
+
+if [ "$license" == "accept" ] || [ "$license" == "accept-no-persist" ]; then
+  read -r -p "Continue with installation? [y/N] " response
+else
+  curl https://raw.githubusercontent.com/habitat-sh/on-prem-builder/master/LICENSE-NOTICE
+  echo
+
+  read -r -p "Do you accept the terms of this license? Answering yes will proceed with the installation. [y/N] " response
+fi
+
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
     pushd scripts > /dev/null
     sudo ./install-hab.sh
