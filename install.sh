e7ac9d7028fe1810f375c4c2c214ad1bee6c3411
Ben Dang <me@bdang.it>
2019-01-16 23:15:10 -0800

    Allows users to install on-prem bldr in containers
    Minimal containers do not have sudo.  This checks for existence of sudo
    otherwise don't execute the commands with sudo.
    
    Signed-off-by: Ben Dang <me@bdang.it>

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 
 umask 0022
 
+sudo () {
+    [[ $EUID = 0 ]] || set -- command sudo -E "$@"
+    "$@"
+}
+
 type curl >/dev/null 2>&1 || { echo >&2 "curl is required for installation of habitat, but was not found. Exiting."; exit 1; }
 
 curl https://raw.githubusercontent.com/habitat-sh/on-prem-builder/master/NOTICE
@@ -10,8 +15,8 @@ echo
 read -r -p "Continue with installation? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
     pushd scripts > /dev/null
-    sudo -E ./install-hab.sh
-    sudo -E ./hab-sup.service.sh
-    sudo -E ./provision.sh
+    sudo ./install-hab.sh
+    sudo ./hab-sup.service.sh
+    sudo ./provision.sh
     popd > /dev/null
 fi
