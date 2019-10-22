05aeae048730ae16b4cf81babf6196cd0c0b3fb1
Jeremy J. Miller <jm@chef.io>
2019-10-22 09:46:06 -0400

    finalized validation of airgap section
    Signed-off-by: Jeremy J. Miller <jm@chef.io>

--- install.sh
+++ install.sh
@@ -7,9 +7,7 @@ sudo () {
     "$@"
 }
 
-type curl >/dev/null 2>&1 || { echo >&2 "curl is required for installation of habitat, but was not found. Exiting."; exit 1; }
-
-curl https://raw.githubusercontent.com/habitat-sh/on-prem-builder/master/NOTICE
+cat NOTICE
 echo
 
 license="${HAB_LICENSE:-}"
@@ -17,7 +15,7 @@ license="${HAB_LICENSE:-}"
 if [ "$license" == "accept" ] || [ "$license" == "accept-no-persist" ]; then
   read -r -p "Continue with installation? [y/N] " response
 else
-  curl https://raw.githubusercontent.com/habitat-sh/on-prem-builder/master/LICENSE-NOTICE
+  cat LICENSE-NOTICE
   echo
 
   read -r -p "Do you accept the terms of this license? Answering yes will proceed with the installation. [y/N] " response
@@ -25,7 +23,9 @@ fi
 
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
     pushd scripts > /dev/null
-    sudo ./install-hab.sh
+    export HAB_LICENSE=accept
+    # use -E to pull in the HAB_LICENSE Env variable
+    sudo -E ./install-hab.sh
     sudo ./hab-sup.service.sh
     sudo ./provision.sh
     popd > /dev/null
--- scripts/install-hab.sh
+++ scripts/install-hab.sh
@@ -2,7 +2,15 @@
 
 set -euo pipefail
 
-type curl >/dev/null 2>&1 || { echo >&2 "curl is required for installation of habitat, but was not found. Exiting."; exit 1; }
-curl https://raw.githubusercontent.com/habitat-sh/habitat/master/components/hab/install.sh | bash
-hab pkg install core/cacerts
-hab pkg install core/hab-sup
+install_hab() {
+  type curl >/dev/null 2>&1 || { echo >&2 "curl is required for installation of habitat, but was not found. Exiting."; exit 1; }
+  curl https://raw.githubusercontent.com/habitat-sh/habitat/master/components/hab/install.sh | bash
+}
+
+install_deps() {
+  hab pkg path core/cacerts >/dev/null 2>&1 || hab pkg install core/cacerts
+  hab pkg path core/hab-sup >/dev/null 2>&1 || hab pkg install core/hab-sup
+}
+
+type hab > /dev/null 2>&1 || install_hab
+install_deps
